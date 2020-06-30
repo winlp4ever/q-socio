652dea9bc7f0b7b05e3f38f976225c0be6710186
ha quang le <winlp4ever@gmail.com>
2020-06-30 09:52:19 +0200

    add infinite scroll feature

--- src/question-list/question-list.js
+++ src/question-list/question-list.js
@@ -3,36 +3,37 @@ import Question from '../question/question'
 import RefreshLoader from '../animation-icon/refresh-loader'
 import './_question-list.scss'
 
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-const QuestionList = (props) => {    
+const QuestionList = (props) => {
+    const [loading, setLoading] = useState(false)
+    const [qs, setQs] = useState([...Array(12).keys()])
+    const fetchData = async () => {
+        console.log('fetching data...')
+        setQs([...qs, 1])
+    }
+    
+    const loadMore = async () => {
+        if (loading) return 
+        setLoading(true)
+        await fetchData()
+        setLoading(false)
+    }
+
     return <div className='question-list'>
-        <RefreshLoader />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
+        <InfiniteScroll
+            dataLength={qs.length}
+            next={loadMore}
+            hasMore={true}
+            loader={<h4>Loading...</h4>}
+            endMessage={
+                <h4>End.</h4>
+            }
+        >
+            <RefreshLoader />
+            {qs.map((_, i) => <Question key={i}/>)}
+        </InfiniteScroll>
+        
     </div>
 }
 
